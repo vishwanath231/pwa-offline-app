c0e3aaca17701d6c1c85c271d6e361320c233c52
vishwanath231 <vishwanatharuchunan@gmail.com>
2025-07-18 08:48:45 +0530

    final2

--- public/sw.js
+++ public/sw.js
@@ -12,15 +12,21 @@ const ASSETS_TO_CACHE = [
   '/logo512.png'
 ];
 
-// Install event - cache files
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.webmanifest',
+        '/favicon.ico',
+      ]);
     })
   );
-  self.skipWaiting();
 });
+
+
 
 // Activate event
 self.addEventListener('activate', (event) => {
@@ -35,19 +41,10 @@ self.addEventListener('activate', (event) => {
 });
 
 // Fetch event
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return (
-        cached ||
-        fetch(event.request).catch(() => {
-          if (event.request.destination === 'document') {
-            return caches.match('/offline.html');
-          }
-        })
-      );
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
